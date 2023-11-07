b786a555a37be9b562276e259524c8566ebf4228
Timi Ajeojo <timiajeojo@gmail.com>
2023-11-07 03:32:40 +0100

    update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
-if (sum < 21) {
+if (sum < 20) {
     console.log("do you want to draw a new card? 🙂");
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳");
+    hasBlackJack = true
 } else if (sum > 21) {
     console.log("You're out of the game! 😭");
 }
