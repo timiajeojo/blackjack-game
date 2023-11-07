87f328f5ec5e977daa75cc1004ecf21d251d4aa4
Timi Ajeojo <timiajeojo@gmail.com>
2023-11-07 03:16:23 +0100

    added js

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstCard = 6
+let secondCard = 9
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("do you want to draw a new card? 🙂");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳");
+} else if (sum > 21) {
+    console.log("You're out of the game! 😭");
+}
